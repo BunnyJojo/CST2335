6c5e4704f3a468cb4fb6eee77025c95c096e538c
lizho <lizhongxuan.sfls@gmail.com>
2017-03-22 17:33:53 -0400

    lab 5 first draft

---
+++ app/src/main/java/com/example/lizho/androidlabs/MessageDetails.java
@@ -0,0 +1,24 @@
+package com.example.lizho.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageDetails extends AppCompatActivity {
+    //private SimpleFragment fragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_details);
+
+       /* SimpleFragment fragment= new SimpleFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString();
+
+            fragment.setArgument(bundle)
+
+        getsupportFragmentManager().beinTransaction().add(R.id.framlayout, fragment).commit();
+
+
+        */
+    }
+}
